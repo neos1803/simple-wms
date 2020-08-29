16204cb8c03b67e0775c7c25b17533f44297a76d
musfirotus <musfirotus@gmail.com>
2020-08-29 20:22:34 +0700

    Rest User

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,10 @@ const UserController = require('../controllers/UserController')
 
 router
   .get('/home', UserController.home)
+  .get('/', UserController.getUsers)
   .get('/:id', UserController.getUser)
+  .post('/', UserController.saveUser)
+  .put('/:id', UserController.updateUser)
+  .delete('/:id', UserController.deleteUser)
 
 module.exports = router
