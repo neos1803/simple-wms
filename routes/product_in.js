c6c8becd6f3ecab85835d6e6488ce8e903feb8e3
musfirotus <musfirotus@gmail.com>
2020-08-30 12:59:26 +0700

    Rest ProductIn

--- routes/product_in.js
+++ routes/product_in.js
@@ -5,6 +5,10 @@ const ProductInController = require('../controllers/ProductInController')
 
 router
   .get('/home', ProductInController.home)
-  .get('/:id', ProductInController.getProductIn)
+  .get('/', ProductInController.getAll)
+  .get('/:id', ProductInController.get)
+  .post('/', ProductInController.save)
+  .put('/:id', ProductInController.update)
+  .delete('/:id', ProductInController.delete)
 
 module.exports = router
