8b46547ad523cbc9a61b14ca547f20686b00b115
Nanda Purba <oliviergiroud1998@gmail.com>
2020-08-30 18:32:29 +0700

    Add alias

--- models/product.js
+++ models/product.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.User, { foreignKey: "user_id" })
+      this.belongsTo(models.User, { foreignKey: "user_id", as: "supplier" })
       this.hasMany(models.Product_In, { foreignKey: "product_id" })
       this.hasMany(models.Product_Out, { foreignKey: "product_id" })
     }
--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.Product, { foreignKey: "user_id" })
+      this.hasMany(models.Product, { foreignKey: "user_id", as: "supplier" })
     }
   };
   User.init({
