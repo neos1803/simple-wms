dd5178867d32e723e5a192a5c4b46ee0bd83397b
musfirotus <musfirotus@gmail.com>
2020-08-30 14:05:26 +0700

    Product Upload

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
+const cloudinary = require('cloudinary').v2
+const cloudConfig = require('../config/cloudinaryConfig')
+cloudinary.config(cloudConfig)
 
-const {
-  User,
-  Product
-} = require("../models")
+const { Product } = require("../models")
 const response = require('../helpers/response')
+const pagination = require('../helpers/pagination')
 
-const attUser = ['full_name', 'username', 'email', 'phone_number', 'role']
 const attProduct = ['name', 'photo_url', 'stock', 'price']
 
 class ProductController {
@@ -15,16 +15,15 @@ class ProductController {
     return res.status(200).json(response("Success", "Sukses akses!", "Hai from Product Controller"))
   }
 
-  // static async getProducts(req, res){}
-
   static async getProduct(req, res) {
     const {
       id
     } = req.params
+
     const productget = await Product.findByPk(
       id, {
         attributes: attProduct,
-        included: [{
+        include: [{
           model: User,
           attributes: attUser
         }]
@@ -42,11 +41,146 @@ class ProductController {
     }
   }
 
-  // static async saveProduct(req, res){}
+  static async saveProduct(req, res){
+    try {
+      const {
+        name,
+        stock,
+        price,
+        user_id
+      } = req.body
 
-  // static async updateProduct(req, res){}
+      // ambil file dari body request
+      const uploadFile = req.files.photo
 
-  // static async deleteProduct(req, res){}
+      // upload pakai cloudinary
+      const image = await cloudinary.uploader.upload(uploadFile.tempFilePath, (err, result) => {
+        console.log(result, err);
+      })
+
+      // tambah product
+      const saveProduct = await Product.create({
+        name,
+        stock,
+        price,
+        user_id,
+        // ambil id dan url dari variabel image
+        photo_id: image.public_id,
+        photo_url: image.secure_url,
+      })
+
+      const datas = {
+        'Nama Product': saveProduct.name,
+        stock: saveProduct.stock,
+        photo_id: saveProduct.photo_id,
+        photo_url: saveProduct.photo_url,
+        price: saveProduct.price,
+        user_id: saveProduct.user_id
+      }
+
+      res.status(201).json(response("Success", "Berhasil tambah data product", datas))
+    } catch (error) {
+      res.status(404).json(response("Failed", error.message))
+    }
+  }
+
+  static async getProducts(req, res){
+    try {
+      const count = await Product.count()
+
+      const page = pagination({
+        limit: req.query.limit,
+        page: parseInt(req.query.page),
+        count: count
+      })
+
+      const show = await Product.findAll({
+        limit: page.limit,
+        offset: page.offset,
+        attributes: attProduct,
+        include: [{
+          model: User,
+          attributes: attUser
+        }]
+      })
+
+      const datas = {
+        data: show,
+        totalItems: page.totalItems,
+        totalPages: page.totalPages,
+        currentPage: page.currentPage
+      }
+      res.status(200).json(response("Success", "Berhasil tampil data", datas))
+    } catch (err) {
+      return res.status(400).json(response("Failed", err))
+    }
+  }
+
+  static async updateProduct(req, res){
+    const {
+      id
+    } = req.params
+
+    const {
+      name,
+      stock,
+      price,
+      user_id
+    } = req.body.data;
+
+    const productUpdate = await Product.update({
+      name,
+      stock,
+      price,
+      user_id
+    }, {
+      where: {
+        id: id
+      }
+    })
+
+    const showProduct = await Product.findByPk(
+      id, {
+        attributes: attProduct,
+        include: [{
+          model: User,
+          attributes: attUser
+        }]
+      }
+    )
+
+    try {
+      if (productUpdate) {
+        return res.status(200).json(response("Success", "Sukses update user!", showProduct))
+      } else {
+        return res.status(400).json(response("Failed!", "Data user tidak ada!", "Kosong"))
+      }
+    } catch (error) {
+      return res.status(400).json(response("Failed", error.message, "Kosong"))
+    }
+  }
+
+  static async deleteProduct(req, res){
+    const {
+      id
+    } = req.params
+
+    const delProduct = await Product.destroy({
+      where: {
+        id: id
+      }
+    })
+
+    try {
+      if (delProduct) {
+        return res.status(200).json(response("Success", "Sukses hapus data user!", `ID : ${id}`))
+      } else {
+        return res.status(400).json(response("Failed", "Data tidak user tidak ada!", `ID : ${id}`))
+      }
+    } catch (error) {
+      return res.status(400).json(response("Failed", error.message, "Kosong"))
+    }
+  }
 }
 
 module.exports = ProductController
